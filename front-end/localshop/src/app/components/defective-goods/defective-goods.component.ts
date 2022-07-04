c5a5a2b745cd9e08b19563acf7391bc3be6c8819
Joy Christine <joychristin2@gmail.com>
2022-07-04 17:22:01 +0300

    add all components

---
+++ front-end/localshop/src/app/components/defective-goods/defective-goods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-defective-goods',
+  templateUrl: './defective-goods.component.html',
+  styleUrls: ['./defective-goods.component.css']
+})
+export class DefectiveGoodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
