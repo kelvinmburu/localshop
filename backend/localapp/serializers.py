eda46ab12fc6fec1ad24694deb881f476d9dc07e
Joy Christine <joychristin2@gmail.com>
2022-07-04 17:25:14 +0300

    added sserializers

---
+++ backend/localapp/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from localapp.models import *
+# ProductSerializer
+# OrderSerializer
+# ClerkSerializer
+# AdminSerializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ClerkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clerk
+        fields = '__all__'
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = ['name', 'email', 'password', 'AdminId']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Product
+        fields = ['name','quantity','category']
+
+
+class DefectiveGoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Product
+        fields = ['name','quantity','category']
