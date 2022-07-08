cb7ea5b41ad687fd740ba2938f3655ed5af7a31b
Kirugik <kirugik79@gmail.com>
2022-07-08 14:47:42 +0300

    Feat: Add new clerks

--- front-end/localshop/src/app/components/clerk/clerk.component.ts
+++ front-end/localshop/src/app/components/clerk/clerk.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-clerk',
   templateUrl: './clerk.component.html',
-  styleUrls: ['./clerk.component.css']
+  styleUrls: ['./clerk.component.css'],
+  providers: [SharedService]  
 })
 export class ClerkComponent implements OnInit {
+  clerkDetails:any;
+  form: any = {
+    fullname: null,
+    email: null,
+    password: null 
+  }; 
 
-  constructor() { }
+  errorMessage = ""
+
+  constructor(private clerkService: SharedService) { }
 
   ngOnInit(): void {
+    this.clerkService.getClerks().subscribe(data => {
+      console.log(data)
+
+      this.clerkDetails = data;
+    });
   }
+
+
+
+  onSubmit(){
+    const { fullname, email, password } = this.form; 
+    console.log(this.form)
+    this.clerkService.registerClerk(fullname, email, password).subscribe(data => {
+        console.log(data);
+      }, err => {
+        this.errorMessage = err.error.message;
+        console.log(this.errorMessage);
+
+      });
+
+      // window.location.href = '/newadmin';
+
+    }
 
 }
