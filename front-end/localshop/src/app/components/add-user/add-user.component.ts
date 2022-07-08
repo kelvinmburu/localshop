b710d33ebb07f32504a3a5195856eae7f08f7657
kelvinmburu <mburuhkelvin@gmail.com>
2022-07-08 15:56:07 +0300

    build & st: updated form with reset button|styled components

--- front-end/localshop/src/app/components/add-user/add-user.component.ts
+++ front-end/localshop/src/app/components/add-user/add-user.component.ts
@@ -1,57 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css'],
-  providers: [SharedService] 
+  providers: [SharedService],
 })
 export class AddUserComponent implements OnInit {
-  adminData:any;
+  adminData: any;
   form: any = {
     username: null,
     email: null,
-    password: null
-  }; 
+    password: null,
+  };
+  errorMessage = '';
 
-  errorMessage = ""
-
-  constructor(private service: SharedService) { }
+  constructor(private service: SharedService) {}
 
   ngOnInit(): void {
-    this.service.getAdmin().subscribe(data => {
-      console.log(data)
+    this.service.getAdmin().subscribe((data) => {
+      console.log(data);
 
       this.adminData = data;
     });
   }
 
-  // addNewAdmin(){
-  //   this.service.addAdmin(this.newAdmin).subscribe(
-  //     response => {
-  //       alert("Admin " + this.newAdmin.username + " added successfully"); 
-  //     } 
-  //     // error => console.log('error', error)
-  //   );
-  // }
-
-
-  onSubmit(){
-    const { username, email, password } = this.form; 
-    console.log(this.form)
-    this.service.addAdmin(username, email, password).subscribe(data => {
+  onSubmit(f: NgForm) {
+    const { username, email, password } = this.form;
+    console.log(this.form);
+    this.service.addAdmin(username, email, password).subscribe(
+      (data) => {
         console.log(data);
-      }, err => {
+      },
+      (err) => {
         this.errorMessage = err.error.message;
         console.log(this.errorMessage);
-
       }
-      )
+    );
 
-      // window.location.href = '/newadmin';
-
-    }
-
-  
+    f.reset()
+  }
 }
