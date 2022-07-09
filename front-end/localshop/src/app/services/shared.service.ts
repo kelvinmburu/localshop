f140959a87d616564cdff3383e63dbd97071873a
kelvinmburu <mburuhkelvin@gmail.com>
2022-07-09 14:09:42 +0300

    build: updated form component

--- front-end/localshop/src/app/services/shared.service.ts
+++ front-end/localshop/src/app/services/shared.service.ts
@@ -1,60 +1,81 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'; //Handle asynchronous requests and responses
 const APIUrl = 'http://127.0.0.1:8000';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-  // readonly APIUrl = 'http://127.0.0.1:8000';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
+  // Admin GET & POST service
+  getAdmin(): Observable<any> {
+    return this.http.get(APIUrl + '/newadmin/');
   }
 
-
-
-
-  // addAdmin(userData:any): Observable<any> {
-  //   return this.http.post(this.APIUrl + '/newadmin/', userData);
-  // }
-
-  getAdmin():Observable<any>{
-    return this.http.get(APIUrl + "/newadmin/"); 
+  addAdmin(name: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      APIUrl + '/newadmin/',
+      { name, email, password },
+      httpOptions
+    );
   }
 
-  addAdmin(name: string, email: string, password: string):Observable<any>{
-    return this.http.post(APIUrl + "/newadmin/", {name, email, password}, httpOptions);
-  } 
-
-
-
-  getClerks():Observable<any>{
-    return this.http.get(APIUrl + "/clerks/"); 
+  // Clerk GET & POST service
+  getClerks(): Observable<any> {
+    return this.http.get(APIUrl + '/clerks/');
   }
 
-  registerClerk(name: string, email: string, password: string):Observable<any>{
-    return this.http.post(APIUrl + "/clerks/", {name, email, password}, httpOptions);
-  } 
+  registerClerk(
+    name: string,
+    email: string,
+    password: string
+  ): Observable<any> {
+    return this.http.post(
+      APIUrl + '/clerks/',
+      { name, email, password },
+      httpOptions
+    );
+  }
 
+  // Product GET & POST service
+  getProductsList(): Observable<any[]> {
+    return this.http.get<any[]>(APIUrl + '/products/');
+  }
 
+  addNewProduct(
+    product_name: string,
+    category: string,
+    quantity: number,
+    status: string,
+    buying_price: number,
+    selling_price: number,
+    expiry_date: Date,
+    date_received: Date
+  ) {
+    return this.http.post(APIUrl + '/products/', {
+      product_name,
+      category,
+      quantity,
+      status,
+      buying_price,
+      selling_price,
+      expiry_date,
+      date_received
+    });
+  }
 
-
-
-
-
-  // // Login API method 
+  // // Login API method
   // loginUser(val:any) {
   //   return this.http.post(this.APIUrl + '/login/', val);
   // }
 
-  
-
-  // // Admin API method 
+  // // Admin API method
   // getAdminList(): Observable<any[]> {
   //   return this.http.get<any[]>(this.APIUrl + '/newadmin/');
   // }
@@ -63,128 +84,86 @@ export class SharedService {
   //   return this.http.put(this.APIUrl + '/newadmin/', val);
   // }
 
-
   // deleteAdmin(val:any) {
   //   return this.http.delete(this.APIUrl + '/newadmin/' + val);
   // }
-
 
   // getAllAdmins():Observable<any[]> {
   //   return this.http.get<any[]>(this.APIUrl + '/newadmin/');
   // }
 
-
-
-
-  // // Products API method 
-  // getProductsList(): Observable<any[]> {
-  //   return this.http.get<any[]>(this.APIUrl + '/products/');
-  // }
-
-
-  // addNewProduct(val:any) {
-  //   return this.http.post(this.APIUrl + '/products/', val);
-  // }
-
-
   // updateProduct(val:any) {
   //   return this.http.put(this.APIUrl + '/products/', val);
   // }
-
 
   // deleteProduct(val:any) {
   //   return this.http.delete(this.APIUrl + '/products/' + val);
   // }
 
-
   // getAllProducts():Observable<any[]> {
   //   return this.http.get<any[]>(this.APIUrl + '/products/');
   // }
 
-
-
-
-  // // Orders API method 
+  // // Orders API method
   // getOrdersList(): Observable<any[]> {
   //   return this.http.get<any[]>(this.APIUrl + '/orders/');
   // }
-
 
   // createOrder(val:any) {
   //   return this.http.post(this.APIUrl + '/orders/', val);
   // }
 
-
   // updateOrder(val:any) {
   //   return this.http.put(this.APIUrl + '/orders/', val);
   // }
-
 
   // deleteOrder(val:any) {
   //   return this.http.delete(this.APIUrl + '/orders/' + val);
   // }
 
-
   // getAllOrders():Observable<any[]> {
-  //   return this.http.get<any[]>(this.APIUrl + '/orders/'); 
+  //   return this.http.get<any[]>(this.APIUrl + '/orders/');
   // }
 
-
-
-
-  // // Stores API method 
+  // // Stores API method
   // getStoresList(): Observable<any[]> {
   //   return this.http.get<any[]>(this.APIUrl + '/stores/');
   // }
-
 
   // createNewStore(val:any) {
   //   return this.http.post(this.APIUrl + '/stores/', val);
   // }
 
-
   // updateStore(val:any) {
   //   return this.http.put(this.APIUrl + '/stores/', val);
   // }
-
 
   // deleteStore(val:any) {
   //   return this.http.delete(this.APIUrl + '/stores/' + val);
   // }
 
-
   // getAllStores():Observable<any[]> {
-  //   return this.http.get<any[]>(this.APIUrl + '/stores/'); 
+  //   return this.http.get<any[]>(this.APIUrl + '/stores/');
   // }
 
-
-
-
-  // // Defective Goods API method 
+  // // Defective Goods API method
   // getDefectiveProductsList(): Observable<any[]> {
   //   return this.http.get<any[]>(this.APIUrl + '/defective/');
   // }
-
 
   // addDefectiveProduct(val:any) {
   //   return this.http.post(this.APIUrl + '/defective/', val);
   // }
 
-
   // updateDefectiveProduct(val:any) {
   //   return this.http.put(this.APIUrl + '/defective/', val);
   // }
-
 
   // deleteDefectiveProduct(val:any) {
   //   return this.http.delete(this.APIUrl + '/defective/' + val);
   // }
 
-
   // getDefectiveProducts():Observable<any[]> {
-  //   return this.http.get<any[]>(this.APIUrl + '/defective/'); 
+  //   return this.http.get<any[]>(this.APIUrl + '/defective/');
   // }
-
-
-
 }
