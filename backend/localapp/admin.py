cbeb3901abc0b3b087e2378d6f4a823835685846
kelvinmburu <mburuhkelvin@gmail.com>
2022-07-14 01:00:43 +0300

    feat: added paths for auth and profile

--- backend/localapp/admin.py
+++ backend/localapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *  
+from rest_framework.authtoken.admin import TokenAdmin
+
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Clerk)
@@ -7,3 +9,5 @@ admin.site.register(Admin)
 admin.site.register(Order)
 # admin.site.register(Store)
 admin.site.register(Defectivegood)
+
+TokenAdmin.raw_id_fields = ['user']
